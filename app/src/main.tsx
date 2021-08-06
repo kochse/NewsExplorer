dd856867377f5c67ff577d202de9af7567e0b257
Sebastian Koch <kochsebastian@gmail.com>
2021-08-06 11:15:22 +0200

    add app

---
+++ app/src/main.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+
+const CounterExample = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  });
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
+}
+
+export default CounterExample
